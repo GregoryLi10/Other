92e3c7686c67ee8a3ca5ea56c0358981d8126fed
GregoryLi10 <90701941+GregoryLi10@users.noreply.github.com>
2022-02-21 12:50:14 -0500

    .

--- Other/src/Other/Logs.java
+++ Other/src/Other/Logs.java
@@ -1,12 +1,17 @@
 package Other;
 
 public class Logs {
-
+	//runs in log of N instead of linear (counting with for loop)
+	public static int lengthOfNum(int n) {
+		return (int) (Math.log10(n)+1);
+	}
+	//similar but a bit faster than dividing until 1
 	public static boolean divisibleBy4(int n) {
 		return Math.log(n)/Math.log(4)%1==0;
 	}
 	
 	public static void main (String[] args) {
 		System.out.println(divisibleBy4(4));
+		System.out.println(lengthOfNum(123));
 	}
 }
